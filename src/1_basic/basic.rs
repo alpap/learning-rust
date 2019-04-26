79a3fcf29f1f66916fd9aaf3e73e053104c0d263
Alexandros Papadopoulos <alpa@hesehus.dk>
2019-04-26 10:09:05 +0200

    checking structs

--- src/1_basic/basic.rs
+++ src/1_basic/basic.rs
@@ -1,6 +1,7 @@
 //! from site https://doc.rust-lang.org/stable/rust-by-example/
 fn main() {
-	hello()
+	hello();
+	literals()
 }
 
 fn hello(){
@@ -39,7 +40,7 @@ fn types(){
     mutable = 21;
 
     // Error! The type of a variable can't be changed.
-    mutable = true;
+    // mutable = true;
 
     // Variables can be overwritten with shadowing.
     let mutable = true;
@@ -68,4 +69,56 @@ fn literals() {
 
     // Use underscores to improve readability!
     println!("One million is written as {}", 1_000_000u32);
+}
+
+fn tuples() {
+    // A tuple with a bunch of different types
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // Values can be extracted from the tuple using tuple indexing
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    // Tuples can be tuple members
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // Tuples are printable
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    // But long Tuples cannot be printed
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
+    // TODO ^ Uncomment the above 2 lines to see the compiler error
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    //tuples can be destructured to create bindings
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+
+}
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    // `let` can be used to bind the members of a tuple to variables
+    let (integer, boolean) = pair;
+
+    (boolean, integer)
 }
